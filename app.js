58b51d9199208d309e62cf88c4dd65ae0f00129f
James Cherven <jcherven@mail.usf.edu>
2019-01-02 20:06:36 -0500

    Adds the scraper logic in in Cheerio

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-const helmet = require('helmet');
-const exphbs = require('express-handlebars');
+var createError = require( 'http-errors' );
+var express = require( 'express' );
+var path = require( 'path' );
+var logger = require( 'morgan' );
+const helmet = require( 'helmet' );
+const exphbs = require( 'express-handlebars' );
 
-const mongoose = require('mongoose');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const mongoose = require( 'mongoose' );
+const axios = require( 'axios' );
+const cheerio = require( 'cheerio' );
 
-const db = require('./models');
+const db = require( './models' );
+
+var indexRouter = require( './routes/fetch-route' );
+
+var app = express();
+app.use( helmet() );
+
+// view engine setup
+app.engine( 'handlebars', exphbs( { defaultLayout: 'main' } ) );
+app.set( 'view engine', 'hbs' );
+
+app.use( logger( 'dev' ) );
+app.use( express.json() );
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+app.use( '/', indexRouter );
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/annStories";
-
-var indexRouter = require('./routes/index');
-
-var app = express();
-app.use(helmet());
-
-// view engine setup
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect( MONGODB_URI, { useNewUrlParser: true } );
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use( function ( req, res, next ) {
+    next( createError( 404 ) );
+} );
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use( function ( err, req, res, next ) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get( 'env' ) === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status( err.status || 500 );
+    res.render( 'error' );
+} );
 
 module.exports = app;
---
+++ routes/fetch-route.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const router = express.Router();
+
+router.get("/fetch-new", (req, res) => {
+    console.log("\n***********************************\n" +
+                "Fetching the front page of ANN News" +
+                "\n***********************************\n");
+
+    const baseUrl = "https://www.animenewsnetwork.com";
+    const newsUrl = "/news";
+
+    axios.get(baseUrl + newsUrl).then(function(response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
+
+        // Cheerio scraping
+        $("div.herald").each((i, element) => {
+            let imageLink = baseUrl + $(element).find("div.thumbnail").attr("data-src");
+            let title = $(element).find("h3").text().trim();
+            let byLine = $(element).find("time").text();
+            let link = baseUrl + newsUrl + $(element).find("div.wrap").find("a").attr("href");
+            let subhead = $(element).find("span.intro").text().trim();
+            let previewText = $(element).find("span.full").text().trim();
+            let articleId = $(element).attr("data-topics");
+
+            // Save these results in an object that we'll push into the results array we defined earlier
+            results.push({
+                articleId: articleId,
+                title: title,
+                imageLink: imageLink,
+                byLine: byLine,
+                link: link,
+                subhead: subhead,
+                previewText: previewText
+            });
+        });
+
+        // Log the results once you've looped through each of the elements found with cheerio
+        //   console.log(results);
+        results.forEach((newsItem) => {
+            console.log(newsItem.articleId);
+            console.log(newsItem.title);
+            console.log(newsItem.byLine);
+            console.log(newsItem.imageLink);
+            console.log(newsItem.subhead + " " + newsItem.previewText);
+            console.log(newsItem.link);
+            console.log("\n");
+        });
+    });
+});
+
+module.exports = router;
